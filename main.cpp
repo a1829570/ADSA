5cea1f949ac097e207aa9e5fdad28aab87725985
Ajaay Sureiander <a1829570@adelaide.edu.au>
2024-08-06 21:13:57 +0930

    adsf

--- main.cpp
+++ main.cpp
@@ -3,31 +3,6 @@
 #include <algorithm>
 #include <cmath>
 
-// Function declarations
-int maxlen(const std::string& I1, const std::string& I2);
-std::string schoolAddition(const std::string& strI1, const std::string& strI2, int B);
-std::string karatsuba(const std::string& a, const std::string& b);
-
-// Helper functions for Karatsuba
-std::pair<std::string, std::string> split(const std::string& num, int mid);
-std::string addStrings(const std::string& num1, const std::string& num2);
-std::string subtractStrings(const std::string& num1, const std::string& num2);
-std::string padZeros(const std::string& str, int numZeros);
-
-int main() {
-    std::string I1, I2;
-    int B;
-
-    std::cin >> I1 >> I2 >> B;
-    
-    std::string sumResult = schoolAddition(I1, I2, B);
-    std::string productResult = karatsuba(I1, I2);
-
-    std::cout << sumResult << " " << productResult << " 0" << std::endl;
-
-    return 0;
-}
-
 int maxlen(const std::string& I1, const std::string& I2) {
     return std::max(I1.length(), I2.length());
 }
@@ -61,80 +36,17 @@ std::string schoolAddition(const std::string& strI1, const std::string& strI2, i
     return result;
 }
 
-std::pair<std::string, std::string> split(const std::string& num, int mid) {
-    int len = num.length();
-    if (mid > len) {
-        mid = len;
-    }
-    return { num.substr(0, mid), num.substr(mid) };
-}
 
-std::string addStrings(const std::string& num1, const std::string& num2) {
-    std::string result;
-    int carry = 0, i = num1.size() - 1, j = num2.size() - 1;
-    while (i >= 0 || j >= 0 || carry) {
-        int sum = carry;
-        if (i >= 0) sum += num1[i--] - '0';
-        if (j >= 0) sum += num2[j--] - '0';
-        result.push_back(sum % 10 + '0');
-        carry = sum / 10;
-    }
-    std::reverse(result.begin(), result.end());
-    return result;
-}
 
-std::string subtractStrings(const std::string& num1, const std::string& num2) {
-    std::string result;
-    int borrow = 0, i = num1.size() - 1, j = num2.size() - 1;
-    while (i >= 0 || j >= 0) {
-        int diff = borrow;
-        if (i >= 0) diff += num1[i--] - '0';
-        if (j >= 0) diff -= num2[j--] - '0';
-        if (diff < 0) {
-            diff += 10;
-            borrow = -1;
-        } else {
-            borrow = 0;
-        }
-        result.push_back(diff + '0');
-    }
-    while (result.size() > 1 && result.back() == '0') {
-        result.pop_back();
-    }
-    std::reverse(result.begin(), result.end());
-    return result;
-}
+int main() {
+    std::string I1, I2;
+    int B;
 
-std::string padZeros(const std::string& str, int numZeros) {
-    return str + std::string(numZeros, '0');
-}
-
-std::string karatsuba(const std::string& a, const std::string& b) {
-    int n = maxlen(a, b);
-    if (n < 4) {
-        return std::to_string(std::stoi(a) * std::stoi(b));
-    }
-
-    // Pad with zeros to make lengths equal
-    std::string a_padded = std::string(n - a.length(), '0') + a;
-    std::string b_padded = std::string(n - b.length(), '0') + b;
-
-    int k = (n + 1) / 2;
-
-    std::pair<std::string, std::string> aSplit = split(a_padded, n - k);
-    std::pair<std::string, std::string> bSplit = split(b_padded, n - k);
-
-    std::string a1 = aSplit.first;
-    std::string a0 = aSplit.second;
-    std::string b1 = bSplit.first;
-    std::string b0 = bSplit.second;
-
-    std::string P0 = karatsuba(a0, b0);
-    std::string P2 = karatsuba(a1, b1);
-    std::string P1 = karatsuba(addStrings(a0, a1), addStrings(b0, b1));
-
-    std::string term1 = padZeros(P2, 2 * k);
-    std::string term2 = padZeros(subtractStrings(subtractStrings(P1, P2), P0), k);
+    std::cin >> I1 >> I2 >> B;
     
-    return addStrings(addStrings(term1, term2), P0);
+    std::string sumResult = schoolAddition(I1, I2, B);
+    std::cout << sumResult << " " << "0" << " 0" << std::endl;
+
+    return 0;
 }
+
